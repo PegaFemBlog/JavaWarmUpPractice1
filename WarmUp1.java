780d9919337d07eb9729d06ee0e28b809918c80b
Rebecca PegaFemBlog <39128621+PegaFemBlog@users.noreply.github.com>
2020-04-03 20:30:42 -0500

    Add files via upload

---
+++ WarmUp1.java
@@ -0,0 +1,32 @@
+//java warm up exercises
+
+int myNumber=5;
+double d=4.5;
+float f = (float)4.253123;
+//can also be cast as
+//float f = 4.253123f;
+
+//set boolean to false
+boolean b=false;
+//then set boolean to true
+b=true;
+
+boolean toBe=false;
+b = toBe
+if (b) {
+  System.out.print(b);
+}
+
+//printing strings
+public class main {
+  public static void main(String[] args) {
+    System.print.println("Hello, world!");
+
+    //constructing a string with other strings
+
+    String s1="This is the first string";
+    String s2=", this is the second string."
+    String s3=s1+s2;
+    System.System.out.print(s3);
+  }
+}
